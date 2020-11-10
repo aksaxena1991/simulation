e0b037111c20da1d018e72d8288a527cf27adc35
Anubhav Saxena <aksaxena1991@gmail.com>
2020-11-11 02:30:15 +0530

    UI changes

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 let init = () =>{
-    let allRadioBtns = $('.radioBtn');
-    allRadioBtns.click(function(){
-        allRadioBtns.removeClass('checked').addClass('unchecked');
+    let wavelengthBtn = $('.wavelengthBtn');
+    wavelengthBtn.click(function(){
+        wavelengthBtn.removeClass('checked').addClass('unchecked');
         $(this).addClass('checked').removeClass('unchecked');
-    })
+    });
+    let frequencyBtn = $('.frequencyBtn');
+    frequencyBtn.click(function(){
+        frequencyBtn.removeClass('checked').addClass('unchecked');
+        $(this).addClass('checked').removeClass('unchecked');
+    });
 }
+// let drag = (event) => {
+//     console.log('anubhav',event);
+//     event.dataTransfer.setData("text", event.target.value);
+// }
+// let drop = (event) => {}
+// let allowDrop = (event) => {
+//     event.preventDefault();
+// }
